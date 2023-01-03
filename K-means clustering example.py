a66cbfeaf72e8198eb094e78da937953a19fa99c
Roy <40725093+roy1337@users.noreply.github.com>
2023-01-03 15:03:23 +0200

    Update K-means clustering example.py

--- K-means clustering example.py
+++ K-means clustering example.py
@@ -20,3 +20,28 @@ customer_data["cluster"] = predictions
 
 # print cluster counts
 print(customer_data["cluster"].value_counts())
+
+
+
+
+
+## Output example 
+
+# customer data
+   customer_id   age  income  spending  cluster
+0             1  37.0  75000.0      20.0        1
+1             2  45.0  50000.0      30.0        3
+2             3  63.0  80000.0      40.0        2
+3             4  25.0  45000.0      35.0        0
+4             5  35.0  60000.0      25.0        1
+
+# cluster counts
+1    2
+3    1
+2    1
+0    1
+Name: cluster, dtype: int64
+
+
+## This shows the original customer data with an added column for the cluster predictions. The cluster counts show that there are 2 customers in cluster 1, 1 customer in cluster 3, 1 customer in cluster 2, and 1 customer in cluster 0.
+## Keep in mind that this is just an example and the actual data and cluster assignments will depend on the specifics of your dataset and the random initialization of the KMeans model.
